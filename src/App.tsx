6ef0b28c7c8dccc67d4fe6d2208be9998216d3d4
Luckillash <lucassalazarc@outlook.com>
2024-10-15 18:25:10 -0300

    LISC: Se ajusta la ruta del logo IO

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,7 @@ function App() {
             <a
 
               
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="*"
               
             target="_blank"
 
